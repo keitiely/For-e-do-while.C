48854068d1d96178f7062f57c7a959d2f05389ba
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-12 22:44:04 -0300

    Update quadrado-vazio.c

--- quadrado-vazio.c
+++ quadrado-vazio.c
@@ -4,31 +4,35 @@ Seu programa deve trabalhar com quadrados de todos os comprimentos de lado poss�
 
 #include <stdio.h>
 int main() {
-    int lado; 
-    printf("Digite o tamanho do lado do quadrado:\n");
-    scanf("%d", &lado);
+    int tamanho; 
+    printf("Digite o tamanho do lado do quadrado: ");
+    scanf("%d", &tamanho);
 
     // Verifica se o tamanho esta entre 1 e 20
-    if (lado < 1 || lado > 20) {
+    if (tamanho < 1 || tamanho > 20) {
         printf("Tamanho do lado nao permitido, insira um número entre 1 e 20.\n");
         return 1; // Retorna um código de erro
     }
 
-    // Imprime o quadrado vazio
-    for (int i = 0; i < lado; i++) {
-        for (int j = 0; j < lado; j++) {
-           
-    // Imprime asterisco no contorno do quadrado
-    //i == 0: Verifica se i está na primeira linha.
-    //i == lado -1: Verifica se i está na ultima linha.
-            if (i == 0 || i == lado - 1 || j == 0 || j == lado - 1) {
-                printf("*");
-            } else {
-                printf(" ");
-            }
-        }
-  printf("\n"); // Move para a próxima linha
-    }
+//primeira linha do quadrado
+for (int i=1; i<=tamanho ; i++){
+       printf(" *");
+}
+ printf("\n");
 
-    return 0; // Retorna sucesso
+//segunda linha do quadrado ate a penultima, o meio do quadrado   
+for (int i=2 ; i<tamanho ; i++){
+printf(" *");               //primeiro asterisco da segunda linha do quadrado
+
+    for (int j=2; j<tamanho ; j++){
+         printf("  ");   //espaco de cada asterisco no meio do quadrado
+      }
+    printf(" * \n");   //ultimo asterisco na linha pulando para a proxima linha
+ }
+
+    for (int i=1; i<=tamanho ; i++){
+         printf(" *");     //ultima linha seguindo mesmo padrao da primeira
+    }
+printf("\n");
+     return 0;
 }
